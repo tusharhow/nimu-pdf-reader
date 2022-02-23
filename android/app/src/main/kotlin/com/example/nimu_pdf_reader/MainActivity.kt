4bee1d50bd5ed8ac19a1928e86143f803b2d02fc
tusharhow <tusharsoftdev@gmail.com>
2022-02-23 07:38:06 -0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/nimu_pdf_reader/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nimu_pdf_reader
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final GlobalKey<SfPdfViewerState> _pdfViewerKey = GlobalKey();
+  PdfViewerController? _pdfViewerController;
+  @override
+  void initState() {
+    super.initState();
+    _pdfViewerController = PdfViewerController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SfPdfViewer.network(
+        'https://cdn.syncfusion.com/content/PDFViewer/flutter-succinctly.pdf',
+        key: _pdfViewerKey,
+        controller: _pdfViewerController,
+        onPageChanged: (page) {
+          print(page.newPageNumber);
+        },
+        currentSearchTextHighlightColor: Colors.red,
+        initialZoomLevel: 1.25,
+      ),
+    );
+  }
+}
